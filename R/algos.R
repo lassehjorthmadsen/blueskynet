8a6558980be7f4025971641013ebcd8f009bdbdb
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2025-11-19 09:47:26 +0100

    Add check for prop <= 0 in create_widget(). Fix code formatting

--- R/algos.R
+++ R/algos.R
@@ -80,27 +80,35 @@
 #' @importFrom cli cli_progress_bar cli_progress_update
 #' @export
 #'
-expand_net <- function(net,
-                       keywords,
-                       token,
-                       refresh_tok,
-                       save_net = FALSE,
-                       file_name,
-                       threshold,
-                       max_iterations,
-                       sample_size = Inf) {
-
+expand_net <- function(
+  net,
+  keywords,
+  token,
+  refresh_tok,
+  save_net = FALSE,
+  file_name,
+  threshold,
+  max_iterations,
+  sample_size = Inf
+) {
   keywords <- paste(keywords, collapse = "|")
   profiles <- dplyr::tibble()
   i <- 1
 
   cat("Initial network:\n")
-  cat("   Number of actors     :", dplyr::n_distinct(net$actor_handle), fill = T)
-  cat("   Number of follows    :", dplyr::n_distinct(net$follows_handle), fill = T)
+  cat(
+    "   Number of actors     :",
+    dplyr::n_distinct(net$actor_handle),
+    fill = T
+  )
+  cat(
+    "   Number of follows    :",
+    dplyr::n_distinct(net$follows_handle),
+    fill = T
+  )
   cat("   Number of connections:", nrow(net), fill = T)
 
   while (i == 1 || nrow(new_follows) > 0 && i <= max_iterations) {
-
     cat("\nExpanding the network, iteration number:", i, fill = T)
     i <- i + 1
     start_time <- Sys.time()
@@ -109,27 +117,41 @@ expand_net <- function(net,
     prospects <- net |>
       dplyr::add_count(.data$follows_handle) |>
       dplyr::mutate(frac = .data$n / dplyr::n_distinct(.data$actor_handle)) |>
-      dplyr::filter(!.data$follows_handle %in% .data$actor_handle, .data$follows_handle != "handle.invalid") |>
+      dplyr::filter(
+        !.data$follows_handle %in% .data$actor_handle,
+        .data$follows_handle != "handle.invalid"
+      ) |>
       dplyr::slice_sample(n = sample_size)
 
     # You're included if you're above the threshold, OR at the maximum value
     if (floor(threshold) > 0) {
-      prospects <- prospects |> dplyr::filter(.data$n >= threshold | .data$n == max(.data$n))
-      } else {
-      prospects <- prospects |> dplyr::filter(.data$frac >= threshold | .data$frac == max(.data$frac))
+      prospects <- prospects |>
+        dplyr::filter(.data$n >= threshold | .data$n == max(.data$n))
+    } else {
+      prospects <- prospects |>
+        dplyr::filter(.data$frac >= threshold | .data$frac == max(.data$frac))
     }
 
     prospects <- prospects |>
       dplyr::pull(.data$follows_handle) |>
       unique()
 
-    cat("   Number of prospects meeting the followers threshold of ", threshold, ": ", length(prospects), sep = "", fill = T)
+    cat(
+      "   Number of prospects meeting the followers threshold of ",
+      threshold,
+      ": ",
+      length(prospects),
+      sep = "",
+      fill = T
+    )
 
     # Get profile information for new prospects, to check description for keywords
     if (nrow(profiles) == 0) {
       prospect_profiles <- prospects |> get_profiles(token)
-      } else {
-      prospect_profiles <- prospects |> setdiff(profiles$handle) |> get_profiles(token)
+    } else {
+      prospect_profiles <- prospects |>
+        setdiff(profiles$handle) |>
+        get_profiles(token)
     }
 
     # Store the profiles we have collected, to avoid having to collect again
@@ -142,38 +164,53 @@ expand_net <- function(net,
     # Filter profiles
     if (nrow(prospect_profiles) > 0) {
       prospect_profiles <- prospect_profiles |>
-      dplyr::filter(.data$handle != "invalid.handle") |>
-      dplyr::filter(stringr::str_detect(tolower(.data$description), keywords))
+        dplyr::filter(.data$handle != "invalid.handle") |>
+        dplyr::filter(stringr::str_detect(tolower(.data$description), keywords))
     }
 
-    cat("   Number of valid prospects also matching the provided keywords: ", nrow(prospect_profiles), sep = "", fill = T)
+    cat(
+      "   Number of valid prospects also matching the provided keywords: ",
+      nrow(prospect_profiles),
+      sep = "",
+      fill = T
+    )
 
     new_follows <- dplyr::tibble()
 
     if (nrow(prospect_profiles) > 0) {
-
       new_members <- prospect_profiles |> dplyr::pull(.data$handle)
-      cli::cli_progress_bar(name = "   Getting follows ", type = "iterator", clear = F, total = length(new_members))
+      cli::cli_progress_bar(
+        name = "   Getting follows ",
+        type = "iterator",
+        clear = F,
+        total = length(new_members)
+      )
 
       for (n in seq(new_members)) {
-
         follows <- get_follows(new_members[n], token)
-        if (!is.data.frame(follows)) follows <- NULL
+        if (!is.data.frame(follows)) {
+          follows <- NULL
+        }
 
         if (!is.null(follows) && nrow(follows) > 0) {
           follows <- follows |>
             dplyr::mutate(actor_handle = new_members[n]) |>
             dplyr::select(.data$actor_handle, follows_handle = .data$handle)
-          }
+        }
 
         new_follows <- dplyr::bind_rows(new_follows, follows)
 
         cli::cli_progress_update()
       }
 
-      cat("   Number of new actors with followers to add to network: ", dplyr::n_distinct(new_follows$actor_handle), sep = "", fill = T)
+      cat(
+        "   Number of new actors with followers to add to network: ",
+        dplyr::n_distinct(new_follows$actor_handle),
+        sep = "",
+        fill = T
+      )
 
-      net <- dplyr::bind_rows(net, new_follows)   # Append the new net to the existing; do another round
+      net <- dplyr::bind_rows(net, new_follows) # Append the new net to the existing; do another round
 
       # Refresh the token; unclear how often we need to do this, for now once per iteration
       # Earlier: once per get_follows() call
@@ -183,15 +220,22 @@ expand_net <- function(net,
     }
 
     cat("\nUpdated network:", fill = T)
-    cat("   Number of actors     :", dplyr::n_distinct(net$actor_handle), fill = T)
-    cat("   Number of follows    :", dplyr::n_distinct(net$follows_handle), fill = T)
+    cat(
+      "   Number of actors     :",
+      dplyr::n_distinct(net$actor_handle),
+      fill = T
+    )
+    cat(
+      "   Number of follows    :",
+      dplyr::n_distinct(net$follows_handle),
+      fill = T
+    )
     cat("   Number of connections:", nrow(net), fill = T)
     cat("\n")
     print(Sys.time() - start_time)
 
     # Save the current net, overwriting earlier versions
     if (save_net) net |> saveRDS(file = file_name)
-
   }
   # Return the expanded net and the new tokens we have obtained underway
   return(list("net" = net, "token" = token, "refresh_tok" = refresh_tok))
@@ -300,7 +344,6 @@ expand_net <- function(net,
 #' @importFrom rlang .data
 #' @export
 trim_net <- function(net, threshold) {
-
   net <- net |>
     dplyr::distinct(.keep_all = TRUE) |>
     dplyr::filter(.data$follows_handle != "handle.invalid") |>
@@ -321,8 +364,10 @@ trim_net <- function(net, threshold) {
     }
 
     net <- net |>
-      dplyr::filter(.data$follows_handle %in% .data$actor_handle,
-                    .data$actor_handle %in% .data$follows_handle) |>
+      dplyr::filter(
+        .data$follows_handle %in% .data$actor_handle,
+        .data$actor_handle %in% .data$follows_handle
+      ) |>
       dplyr::select(-.data$n, -.data$frac)
   }
 
@@ -379,7 +424,6 @@ trim_net <- function(net, threshold) {
 #' @export
 #'
 init_net <- function(key_actors, keywords, token) {
-
   keywords <- paste(keywords, collapse = "|")
 
   net <- key_actors |>
@@ -398,7 +442,9 @@ init_net <- function(key_actors, keywords, token) {
   profiles |>
     dplyr::filter(stringr::str_detect(tolower(.data$description), keywords))
 
-  if (nrow(profiles) == 0) stop("Failed to initialize net; no follows that matches keywords")
+  if (nrow(profiles) == 0) {
+    stop("Failed to initialize net; no follows that matches keywords")
+  }
 
   net <- net |>
     dplyr::filter(.data$follows_handle %in% profiles$handle)
@@ -505,7 +551,6 @@ init_net <- function(key_actors, keywords, token) {
 #'
 #' @export
 add_metrics <- function(profiles, net) {
-
   # Compute centrality metrics
   # 1. graph object
   graph <- net |> tidygraph::as_tbl_graph()
@@ -531,7 +576,6 @@ add_metrics <- function(profiles, net) {
     dplyr::rename(handle = .data$name)
 
   if (!is.null(profiles)) {
-
     profiles <- profiles |>
       dplyr::left_join(metrics, by = "handle") |>
       dplyr::left_join(followers, by = c("handle" = "follows_handle"))
@@ -640,10 +684,17 @@ add_metrics <- function(profiles, net) {
 #' @importFrom tidygraph activate as_tbl_graph
 #' @export
 create_widget <- function(net, profiles, prop = 1) {
+  if (prop <= 0) {
+    return(NULL)
+  }
 
   if (prop < 1) {
     profiles <- profiles |> dplyr::slice_sample(prop = prop)
-    net <- net |> dplyr::filter(.data$actor_handle %in% profiles$handle, .data$follows_handle %in% profiles$handle)
+    net <- net |>
+      dplyr::filter(
+        .data$actor_handle %in% profiles$handle,
+        .data$follows_handle %in% profiles$handle
+      )
   }
 
   # Create graph object
@@ -660,18 +711,25 @@ create_widget <- function(net, profiles, prop = 1) {
 
   nodes <- graph |>
     tidygraph::activate(nodes) |>
-    dplyr::mutate(id = dplyr::row_number()) |> dplyr::as_tibble()
+    dplyr::mutate(id = dplyr::row_number()) |>
+    dplyr::as_tibble()
 
   # Make labels for nodes
   nodes <- nodes |>
-    dplyr::mutate(com_label = forcats::fct_infreq(as.character(community)),
-                  com_label = forcats::fct_lump(.data$com_label, n = 10)) |>
+    dplyr::mutate(
+      com_label = forcats::fct_infreq(as.character(community)),
+      com_label = forcats::fct_lump(.data$com_label, n = 10)
+    ) |>
     dplyr::group_by(.data$com_label) |>
-    dplyr::mutate(n = dplyr::n(),
-                  com_id = dplyr::cur_group_id()) |>
+    dplyr::mutate(n = dplyr::n(), com_id = dplyr::cur_group_id()) |>
     dplyr::ungroup() |>
-    dplyr::mutate(com_label = ifelse(.data$com_id == max(.data$com_id), "Other",
-                              paste0(LETTERS[.data$com_id], ", n=", .data$n))) |>
+    dplyr::mutate(
+      com_label = ifelse(
+        .data$com_id == max(.data$com_id),
+        "Other",
+        paste0(LETTERS[.data$com_id], ", n=", .data$n)
+      )
+    ) |>
     dplyr::left_join(profiles, by = c("name" = "handle"))
 
   # Make colors based on communities
@@ -687,14 +745,17 @@ create_widget <- function(net, profiles, prop = 1) {
     dplyr::mutate(color = use_colors[.data$com_id], groupname = .data$com_label)
 
   # 3d plot with threejs
-  widget <- threejs::graphjs(graph, bg = "black",
-                             vertex.size = 0.2,
-                             edge.width = .3,
-                             edge.alpha = .3,
-                             vertex.color = nodes$color,
-                             vertex.label = paste(nodes$displayName, nodes$description, sep = ": "))
+  widget <- threejs::graphjs(
+    graph,
+    bg = "black",
+    vertex.size = 0.2,
+    edge.width = .3,
+    edge.alpha = .3,
+    vertex.color = nodes$color,
+    vertex.label = paste(nodes$displayName, nodes$description, sep = ": ")
+  )
 
-    return(widget)
+  return(widget)
 }
 
 #' Build complete network analysis pipeline
@@ -769,20 +830,29 @@ create_widget <- function(net, profiles, prop = 1) {
 #'
 #' @export
 #'
-build_network <- function(key_actors, keywords, token, refresh_tok, threshold, prop, ...) {
-
+build_network <- function(
+  key_actors,
+  keywords,
+  token,
+  refresh_tok,
+  threshold,
+  prop,
+  ...
+) {
   keywords <- keywords |> paste0(collapse = "|")
 
   # Get initial net based on key actor
   small_net <- init_net(key_actors, keywords, token)
 
   # Expand the net
-  expnet <- expand_net(net = small_net,
-                       keywords = keywords,
-                       token = token,
-                       refresh_tok = refresh_tok,
-                       threshold = threshold,
-                       ...)
+  expnet <- expand_net(
+    net = small_net,
+    keywords = keywords,
+    token = token,
+    refresh_tok = refresh_tok,
+    threshold = threshold,
+    ...
+  )
 
   # Get the expanded network and token (it might have been refreshed)
   token <- expnet$token
@@ -803,12 +873,16 @@ build_network <- function(key_actors, keywords, token, refresh_tok, threshold, p
   # Create widget
   widget <- create_widget(net, profiles, prop)
 
-  return(list("net" = net, "profiles" = profiles, "widget" = widget, "freqs" = freqs))
+  return(list(
+    "net" = net,
+    "profiles" = profiles,
+    "widget" = widget,
+    "freqs" = freqs
+  ))
 }
 
 
 # Get the top 3 words (tf-idf-weighted) per community, use as labels
-
 
 #' Generate labels for network communities
 #'
@@ -872,7 +946,10 @@ com_labels <- function(profiles, top = 3) {
 
       # Create tokens and dfm
       tokens_obj <- quanteda::tokens(corpus_obj, remove_punct = TRUE)
-      tokens_clean <- quanteda::tokens_remove(tokens_obj, pattern = c(quanteda::stopwords("en"), "|"))
+      tokens_clean <- quanteda::tokens_remove(
+        tokens_obj,
+        pattern = c(quanteda::stopwords("en"), "|")
+      )
       dfm_obj <- quanteda::dfm(tokens_clean)
       dfm_tfidf <- quanteda::dfm_tfidf(dfm_obj)
 
